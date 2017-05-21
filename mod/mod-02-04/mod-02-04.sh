33f01379f9fdcfc4c970e7873bf5d22a1e4fa791
Jason Hsu <jhsu802701@jasonhsu.com>
2017-05-21 17:14:16 +0000

    Gave Unit 2 Chapter 4 its own branch

--- mod/mod-02-04/mod-02-04.sh
+++ mod/mod-02-04/mod-02-04.sh
@@ -11,7 +11,11 @@ echo '###########################################################'
 echo 'Unit 2 Chapter 4: Restricting Dangerous Tasks in Production'
 echo '###########################################################'
 
+git checkout -b 02-04-restrict_dangerous_tasks
+
 mv mod-02-04-disable.rake lib/tasks/disable_db_tasks_on_production.rake
 
 git add .
 git commit -m "Restrict dangerous tasks in production"
+git checkout master
+git merge 02-04-restrict_dangerous_tasks
