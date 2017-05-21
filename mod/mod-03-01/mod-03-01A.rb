c8c06ff2daf3a38f3b8797bdd7fda19de513fb07
Jason Hsu <jhsu802701@jasonhsu.com>
2017-05-21 06:38:34 +0000

    Removed Ruby scripts A, B, and C from Unit 3 Chapter 1

--- mod/mod-03-01/mod-03-01A.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-
-require 'insert_from_file'
-require 'gemfile_entry'
-
-InsertFromFile.add_end('mod-03-01-add_to_Gemfile.txt', 'Gemfile')
-StringInFile.replace("gem 'minitest'", "#{GemfileEntry.latest('minitest')}", 'Gemfile')
-StringInFile.replace("gem 'minitest-reporters'", "#{GemfileEntry.latest('minitest-reporters')}", 'Gemfile')
--- mod/mod-03-01/mod-03-01C.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby
-
-require 'insert_from_file'
-
-InsertFromFile.add_after('mod-03-01-test1.txt', 'test/integration/test1_test.rb', 'class Test1Test < ActionDispatch::IntegrationTest')
