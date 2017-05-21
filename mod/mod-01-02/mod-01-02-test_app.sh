d3a710cc53b7d1adc650ffa6fc8e07d473740fbd
Jason Hsu <jhsu802701@jasonhsu.com>
2017-05-21 06:52:34 +0000

    Removed pg-start.sh from test_app.sh

--- mod/mod-01-02/mod-01-02-test_app.sh
+++ mod/mod-01-02/mod-01-02-test_app.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Starting the PostgreSQL server may not always be necessary,
-# but it's better to include this step and not need it
-# than to lack it when it's necessary.
-sh pg-start.sh
-
 echo '--------------'
 echo 'bundle install'
 bundle install
