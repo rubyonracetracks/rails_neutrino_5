0d74025b8b80b0a301677c51230b34b12e8b86d0
Jason Hsu <jhsu802701@jasonhsu.com>
2017-05-24 22:20:12 +0000

    latest->active in Unit 1 Chapter 4

--- mod/mod-01-04/mod-01-04.rb
+++ mod/mod-01-04/mod-01-04.rb
@@ -6,12 +6,18 @@ require 'gemfile_entry'
 require 'string_in_file'
 
 puts 'Updating the Gemfile'
-LineContaining.add_before('rubocop', "  #{GemfileEntry.latest('brakeman')}", "Gemfile")
-LineContaining.add_before('rubocop', "  #{GemfileEntry.latest('bundler-audit')}", "Gemfile")
-LineContaining.add_before('rubocop', "  #{GemfileEntry.latest('gemsurance')}", "Gemfile")
-LineContaining.add_before('rubocop', "  #{GemfileEntry.latest('rails_best_practices')}", "Gemfile")
-LineContaining.add_after('rubocop', "  #{GemfileEntry.latest('sandi_meter')}", "Gemfile")
-
+LineContaining.add_before('rubocop', "  gem 'brakeman'", "Gemfile")
+LineContaining.add_before('rubocop', "  gem 'bundler-audit'", "Gemfile")
+LineContaining.add_before('rubocop', "  gem 'gemsurance'", "Gemfile")
+LineContaining.add_before('rubocop', "  gem 'rails_best_practices'", "Gemfile")
+LineContaining.add_after('rubocop', "  gem 'sandi_meter'", "Gemfile")
+puts 'bundle install --quiet'
+system('bundle install --quiet')
+StringInFile.replace("gem 'brakeman'", GemfileEntry.active('brakeman'),'Gemfile')
+StringInFile.replace("gem 'bundler-audit'", GemfileEntry.active('bundler-audit'),'Gemfile')
+StringInFile.replace("gem 'gemsurance'", GemfileEntry.active('gemsurance'),'Gemfile')
+StringInFile.replace("gem 'rails_best_practices'", GemfileEntry.active('rails_best_practices'),'Gemfile')
+StringInFile.replace("gem 'sandi_meter'", GemfileEntry.active('sandi_meter'),'Gemfile')
 puts 'bundle install --quiet'
 system('bundle install --quiet')
 
