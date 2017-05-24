f052fbad71f29ae71865bbcd575c6a6e4400369b
Jason Hsu <jhsu802701@jasonhsu.com>
2017-05-24 22:27:28 +0000

    latest -> active in Unit 3 Chapter 1

--- mod/mod-03-01/mod-03-01.rb
+++ mod/mod-03-01/mod-03-01.rb
@@ -6,8 +6,8 @@ require 'string_in_file'
 
 puts 'Adding minitest and minitest-reporters to the Gemfile'
 InsertFromFile.add_end('mod-03-01-add_to_Gemfile.txt', 'Gemfile')
-StringInFile.replace("gem 'minitest'", "#{GemfileEntry.latest('minitest')}", 'Gemfile')
-StringInFile.replace("gem 'minitest-reporters'", "#{GemfileEntry.latest('minitest-reporters')}", 'Gemfile')
+StringInFile.replace("gem 'minitest'", "#{GemfileEntry.active('minitest')}", 'Gemfile')
+StringInFile.replace("gem 'minitest-reporters'", "#{GemfileEntry.active('minitest-reporters')}", 'Gemfile')
 puts 'bundle install --quiet'
 system('bundle install --quiet')
 
