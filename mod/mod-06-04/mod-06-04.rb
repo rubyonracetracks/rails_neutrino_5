305ac41a6bca942ae5703146055b5bc81c84e02b
Jason Hsu <jhsu802701@jasonhsu.com>
2017-05-25 18:14:45 +0000

    Pinned gems added in Unit 6 Chapter 4

--- mod/mod-06-04/mod-06-04.rb
+++ mod/mod-06-04/mod-06-04.rb
@@ -6,7 +6,9 @@ require 'gemfile_entry'
 
 puts 'Adding better_errors and binding_of_caller to the Gemfile'
 InsertFromFile.add_end('mod-06-04-Gemfile.txt', 'Gemfile')
+puts 'bundle install --quiet'
 system('bundle install --quiet')
-bundle install --quiet
-StringInFile.replace("gem 'better_errors'", GemfileEntry.active('better_errors'), 'Gemfile')
-StringInFile.replace("gem 'binding_of_caller'", GemfileEntry.active('binding_of_caller'), 'Gemfile')
+StringInFile.replace("gem 'better_errors'", "#{GemfileEntry.active('better_errors')}", 'Gemfile')
+StringInFile.replace("gem 'binding_of_caller'", "#{GemfileEntry.active('binding_of_caller')}", 'Gemfile')
+puts 'bundle install --quiet'
+system('bundle install --quiet')
