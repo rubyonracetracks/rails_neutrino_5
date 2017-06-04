15ff32b5eb19ba34093943132e6512461f69ba80
Jason Hsu <jhsu802701@jasonhsu.com>
2017-06-04 16:38:34 +0000

    Finished Unit 7 Chapter 4

--- mod/mod-07-04/mod-07-04.rb
+++ mod/mod-07-04/mod-07-04.rb
@@ -12,6 +12,10 @@ puts 'Filling in integration tests'
 LineContaining.delete('  # end', 'test/integration/user_password_reset_test.rb')
 LineContaining.delete_between('class UserPasswordResetTest < ActionDispatch::IntegrationTest', 'end', 'test/integration/user_password_reset_test.rb')
 InsertFromFile.add_after('mod-07-04-user_password_reset_test.txt', 'test/integration/user_password_reset_test.rb', 'class UserPasswordResetTest < ActionDispatch::IntegrationTest')
+StringInFile.add_beginning("# rubocop:disable Metrics/MethodLength\n", 'test/integration/user_password_reset_test.rb')
+StringInFile.add_beginning("# rubocop:disable Metrics/AbcSize\n", 'test/integration/user_password_reset_test.rb')
+StringInFile.add_end("# rubocop:enable Metrics/AbcSize\n", 'test/integration/user_password_reset_test.rb')
+StringInFile.add_end("# rubocop:enable Metrics/MethodLength\n", 'test/integration/user_password_reset_test.rb')
 
 puts 'Updating test/test_helper.rb'
 InsertFromFile.add_end('mod-07-04-test_helper.txt', 'test/test_helper.rb')
@@ -24,3 +28,9 @@ system('mv mod-07-04-new.html.erb app/views/users/passwords/new.html.erb')
 
 puts 'Updating app/views/users/passwords/edit.html.erb'
 system('mv mod-07-04-edit.html.erb app/views/users/passwords/edit.html.erb')
+
+puts 'Updating .rubocop.yml'
+InsertFromFile.add_end('mod-07-04-rubocop.txt', '.rubocop.yml')
+
+puts 'Updating config/rails_best_practices.yml'
+LineContaining.replace('RemoveTrailingWhitespaceCheck', "RemoveTrailingWhitespaceCheck: { ignored_files: ['config/routes.rb'] }", 'config/rails_best_practices.yml')
