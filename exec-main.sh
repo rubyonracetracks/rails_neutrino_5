3ae346174a93328fcb3ef80758b1323b9fe668ee
Jason Hsu <jhsu802701@jasonhsu.com>
2017-09-20 16:11:32 +0000

    sh -> bash

--- exec-main.sh
+++ exec-main.sh
@@ -55,68 +55,68 @@ cp $DIR_MAIN/mod_app.sh $DIR_APP
 cp -R $DIR_MAIN/mod $DIR_APP
 
 # Modify the new app
-cd $DIR_APP && sh mod_app.sh '01-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '01-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '01-03' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '01-04' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '01-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '01-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '01-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '01-04' $TOGGLE_OUTLINE
 
-cd $DIR_APP && sh mod_app.sh '02-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '02-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '02-03' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '02-04' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '02-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '02-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '02-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '02-04' $TOGGLE_OUTLINE
 
-cd $DIR_APP && sh mod_app.sh '03-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '03-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '03-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '03-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '03-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '03-03' $TOGGLE_OUTLINE
 
-cd $DIR_APP && sh mod_app.sh '04-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '04-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '04-03' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '04-04' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '04-05' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '04-06' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '04-07' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '04-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '04-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '04-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '04-04' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '04-05' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '04-06' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '04-07' $TOGGLE_OUTLINE
 
-cd $DIR_APP && sh mod_app.sh '05-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '05-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '05-03' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '05-04' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '05-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '05-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '05-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '05-04' $TOGGLE_OUTLINE
 
-cd $DIR_APP && sh mod_app.sh '06-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '06-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '06-03' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '06-04' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '06-05' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '06-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '06-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '06-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '06-04' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '06-05' $TOGGLE_OUTLINE
 
-cd $DIR_APP && sh mod_app.sh '07-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '07-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '07-03' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '07-04' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '07-05' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '07-06' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '07-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '07-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '07-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '07-04' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '07-05' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '07-06' $TOGGLE_OUTLINE
 
-cd $DIR_APP && sh mod_app.sh '08-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '08-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '08-03' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '08-04' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '08-05' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '08-06' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '08-07' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '08-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '08-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '08-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '08-04' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '08-05' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '08-06' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '08-07' $TOGGLE_OUTLINE
 
-cd $DIR_APP && sh mod_app.sh '09-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '09-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '09-03' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '09-04' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '09-05' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '09-06' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '09-07' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '09-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '09-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '09-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '09-04' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '09-05' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '09-06' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '09-07' $TOGGLE_OUTLINE
 
-cd $DIR_APP && sh mod_app.sh '10-01' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '10-02' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '10-03' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '10-04' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '10-05' $TOGGLE_OUTLINE
-cd $DIR_APP && sh mod_app.sh '10-06' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '10-01' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '10-02' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '10-03' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '10-04' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '10-05' $TOGGLE_OUTLINE
+cd $DIR_APP && bash mod_app.sh '10-06' $TOGGLE_OUTLINE
 
 # Remove the mod* files from the new app
 rm -rf $DIR_APP/mod
