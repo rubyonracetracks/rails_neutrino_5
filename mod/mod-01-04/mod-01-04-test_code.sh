1a4dc4875f5baadde5af7c4fd349bbb2b6280838
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-01-20 15:39:52 -0600

    Updated 01-04; moved enabling of SSL from 02-03 to 01-04

--- mod/mod-01-04/mod-01-04-test_code.sh
+++ mod/mod-01-04/mod-01-04-test_code.sh
@@ -3,49 +3,12 @@
 # This script runs the app through code metrics.
 # Violations will not stop the app from passing but will be flagged here.
 
-echo '----------------------'
-echo 'bundle install --quiet'
-bundle install --quiet
+# bin/daudit, bin/dbrakeman, bin/dgemsurance, bin/drbp, and bin/dsandi
 
-# Checks for security vulnerabilities
-# -A: runs all checks
-# -q: output the report only; suppress information warnings
-# -w2: level 2 warnings (medium and high only)
-echo '---------------------------------------'
-echo 'bundle exec brakeman -Aq -w2 --no-pager'
-bundle exec brakeman -Aq -w2 --no-pager
-
-# Checks for compliance with Sandi Metz' four rules
-echo '-----------------------'
-echo 'bundle exec sandi_meter'
-bundle exec sandi_meter
-
-# Update the local ruby-advisory-db advisory database
-echo '-------------------------------'
-echo 'bundle exec bundle-audit update'
-bundle exec bundle-audit update
-
-# Audit the gems listed in Gemfile.lock for vulnerabilities
-echo '------------------------'
-echo 'bundle exec bundle-audit'
-bundle exec bundle-audit
-
-# Checks for violations of the Ruby Style Guide, not recommended for legacy apps
-echo '----------------------'
-echo 'bundle exec rubocop -D'
-bundle exec rubocop -D
-
-# Checks the quality of Rails code, not recommended for legacy apps
-echo '----------------------------------'
-echo 'bundle exec rails_best_practices .'
-bundle exec rails_best_practices .
-
-# Checks for outdated and insecure gems
-echo '----------------------'
-echo 'bundle exec gemsurance'
-echo 'NOTE: Gemsurance fails intermittently.'
-echo 'If necessary, enter the command "bundle exec gemsurance".'
-bundle exec gemsurance
-wait
-mv gemsurance_report.html log/gemsurance_report.html
-echo 'The Gemsurance Report is at log/gemsurance_report.html .'
+bin/dbundle
+bin/dbrakeman
+bin/dsandi
+bin/daudit
+bin/dcop
+bin/drbp
+bin/dgemsurance
--- mod/mod-02-03/mod-02-03.sh
+++ mod/mod-02-03/mod-02-03.sh
@@ -2,10 +2,6 @@
 set -e
 
 # AGENDA
-# * Configure the production environment to automatically enable SSL.
-#   Do this by uncommenting "config.force_ssl = true".
-#   (When you view your app on the Heroku site, the URL will contain
-#   "https://", and the lock will appear in the address bar.)
 # * Replace Heroku's default WEBrick server with Puma, which can handle
 #   far more web traffic.  Do this by updating the config/puma.rb file
 #   and by adding the Procfile and specifying Puma in it.
@@ -16,8 +12,6 @@ echo '#####################################################'
 
 git checkout -b 02-03-production_parameters
 
-ruby mod-02-03.rb
-
 # WICHTIG/LEGACY/production: update config/puma.rb (if necessary)
 echo 'Updating config/puma.rb'
 mv mod-02-03-puma.rb config/puma.rb
@@ -27,6 +21,6 @@ echo 'Specifying the use of Puma in the Procfile'
 mv mod-02-03-Procfile Procfile
 
 git add .
-git commit -m "Using SSL and Puma in the production environment"
+git commit -m "Using Puma in the production environment"
 git checkout master
 git merge 02-03-production_parameters
