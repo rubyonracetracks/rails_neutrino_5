5cbc78ff33147fa60c4ba913bd56fffb2436c972
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-01-20 20:27:50 -0600

    Added screen output in travis-after.sh

--- travis-after.sh
+++ travis-after.sh
@@ -5,10 +5,36 @@
 
 cd tmp1
 bundle install --quiet
-rails db:migrate && rails test
+rails db:migrate
+
+echo '----------'
+echo 'rails test'
+rails test
+
+echo '---------------------------'
+echo 'brakeman -Aq -w2 --no-pager'
 brakeman -Aq -w2 --no-pager
+
+echo '-----------'
+echo 'sandi_meter'
 sandi_meter
-bundle-audit update && bundle exec bundle-audit
+
+echo '-------------------'
+echo 'bundle-audit update'
+bundle-audit update
+
+echo '------------'
+echo 'bundle-audit'
+bundle-audit
+
+echo '----------'
+echo 'rubocop -D'
 rubocop -D
+
+echo '----------------------'
+echo 'rails_best_practices .'
 rails_best_practices .
+
+echo '----------'
+echo 'gemsurance'
 gemsurance
