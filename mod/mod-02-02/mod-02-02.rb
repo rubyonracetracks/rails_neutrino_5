c374773ad989d1aa9a7bf7121b3682e5a0bbcf4d
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-01-23 01:17:05 -0600

    Deleted the old 02-02; moved 02-04 to 02-02; updated to be Rubocop-compliant

--- mod/mod-02-02/mod-02-02.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ruby
-
-require 'insert_from_file'
-
-# WICHTIG/LEGACY/bash: add the Heroku commands to credentials.sh (if necessary)
-InsertFromFile.add_end('mod-02-02-add_to_credentials_script.txt', 'credentials.sh')
