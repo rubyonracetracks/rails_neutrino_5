7027f26c522822ad82b142bf420ceabed864d4e0
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-03-18 22:55:32 -0500

    Removed mailcatcher.sh

--- mod/mod-04-03/mod-04-03.rb
+++ mod/mod-04-03/mod-04-03.rb
@@ -3,9 +3,6 @@
 require 'insert_from_file'
 require 'string_in_file'
 
-# WICHTIG/LEGACY/recommended: add mailcatcher.sh
-system('mv mod-04-03-mailcatcher.sh mailcatcher.sh')
-
 # WICHTIG/LEGACY/recommended: update config/environments/development.rb if not already done
 InsertFromFile.add_after('mod-04-03-development.txt', 'config/environments/development.rb', 'Rails.application.configure')
 
