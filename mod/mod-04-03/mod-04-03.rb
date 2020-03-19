dc933d9f5a69073432708bc8a31dfc8c9b504ae2
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-03-18 22:53:23 -0500

    Mailcatcher finally works

--- mod/mod-04-03/mod-04-03.rb
+++ mod/mod-04-03/mod-04-03.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/ruby
 
 require 'insert_from_file'
+require 'string_in_file'
 
 # WICHTIG/LEGACY/recommended: add mailcatcher.sh
 system('mv mod-04-03-mailcatcher.sh mailcatcher.sh')
 
 # WICHTIG/LEGACY/recommended: update config/environments/development.rb if not already done
 InsertFromFile.add_after('mod-04-03-development.txt', 'config/environments/development.rb', 'Rails.application.configure')
+
+# WICHTIG/LEGACY/recommended: update docker-compose.yml if not already done
+StringInFile.replace('#MAILCATCHER#', '', 'docker-compose.yml')
