902e00569cbce2c439ac6d2801bc66604775cf69
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-03-20 01:08:03 -0500

    Show Gemfile after adding pry-rails

--- mod/mod-04-05/mod-04-05.rb
+++ mod/mod-04-05/mod-04-05.rb
@@ -7,6 +7,7 @@ require 'gemfile_entry'
 # WICHTIG/LEGACY/recommended: add pry-rails to the Gemfile
 puts 'Adding pry-rails to the Gemfile'
 InsertFromFile.add_end('mod-04-05-Gemfile.txt', 'Gemfile')
+puts Gemfile
 puts 'bundle install --quiet'
 system('bundle install --quiet')
 StringInFile.replace("gem 'pry-rails'", "#{GemfileEntry.active('pry-rails')}", 'Gemfile')
