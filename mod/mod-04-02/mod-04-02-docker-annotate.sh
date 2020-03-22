c53ba8aed1a13318c4c7fdce0f57dde6c1d6cca0
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-03-21 22:38:21 -0500

    Updated annotation in 06-02

--- mod/mod-04-02/mod-04-02-docker-annotate.sh
+++ mod/mod-04-02/mod-04-02-docker-annotate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# NOTE: This script is to be used ONLY in a Docker container or in Travis.
+
 echo '-----------------------------------------'
 echo 'bundle exec annotate -r --models -a -k -i'
 bundle exec annotate -r --models -a -k -i
